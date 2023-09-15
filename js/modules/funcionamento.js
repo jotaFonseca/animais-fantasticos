6328653f6f416bfc3ba08514eeac13594cc99c1b
João Paulo Araújo <jpf.araujo99@hotmail.com>
2023-09-15 18:53:46 -0300

    modificacao em alguns arquivos

--- js/modules/funcionamento.js
+++ js/modules/funcionamento.js
@@ -1,17 +1,17 @@
 export default function Funcionamneto() {
-    const funcionamento = document.querySelector('[data-semana]');
-    const diasSemana = funcionamento.dataset.semana.split(',').map(Number);
-    const horarioSemana = funcionamento.dataset.horario.split(',').map(Number);
-    
-    const dataAgora = new Date();
-    const diaAgora = dataAgora.getDay();
-    const horarioAgora = dataAgora.getHours();
+  const funcionamento = document.querySelector("[data-semana]");
+  const diasSemana = funcionamento.dataset.semana.split(",").map(Number);
+  const horarioSemana = funcionamento.dataset.horario.split(",").map(Number);
 
-    const semanaAberto = diasSemana.indexOf(diaAgora) !== -1;
+  const dataAgora = new Date();
+  const diaAgora = dataAgora.getDay();
+  const horarioAgora = dataAgora.getHours();
 
-    const horarioAberto = (horarioAgora >= horarioSemana[0] && horarioAgora < horarioSemana[1]);
+  const semanaAberto = diasSemana.indexOf(diaAgora) !== -1;
 
-    if(semanaAberto && horarioAberto) {
-        funcionamento.classList.add('aberto');
-    }
+  const horarioAberto = horarioAgora >= horarioSemana[0] && horarioAgora < horarioSemana[1];
+
+  if (semanaAberto && horarioAberto) {
+    funcionamento.classList.add("aberto");
+  }
 }
